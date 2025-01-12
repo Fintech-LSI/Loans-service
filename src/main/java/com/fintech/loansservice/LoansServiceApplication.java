8d5d30ae5430183a56efbc32deaf85eabf5ee31a
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2025-01-12 14:22:02 +0100

    add loans to get user

--- src/main/java/com/fintech/loansservice/LoansServiceApplication.java
+++ src/main/java/com/fintech/loansservice/LoansServiceApplication.java
@@ -2,6 +2,7 @@ package com.fintech.loansservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class LoansServiceApplication {
