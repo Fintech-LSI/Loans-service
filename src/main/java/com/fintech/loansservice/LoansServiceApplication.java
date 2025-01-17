817bcc9e10d418bb422061b6580ab8636459562a
hodaifa <150153831+hodaifa-ech@users.noreply.github.com>
2025-01-18 00:23:24 +0100

    test

--- src/main/java/com/fintech/loansservice/LoansServiceApplication.java
+++ src/main/java/com/fintech/loansservice/LoansServiceApplication.java
@@ -2,7 +2,6 @@ package com.fintech.loansservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class LoansServiceApplication {
